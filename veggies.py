c00ef708aee7bf6e8c08d2e1f89b70bf50795315
deepakk <deepaksingla123@gmail.com>
2022-04-16 22:02:12 +0300

    calculate veggie cost

--- veggies.py
+++ veggies.py
@@ -1,1 +1,10 @@
-veggies = ['cabbage', 'carrot', 'spinach', 'asparagus', 'artichoke', 'lettuce']
+veg_dict = {'cabbage': 2.87, 'carrot': 1.59, 'spinach': 3.33, 'asparagus': 2.54, 'artichoke': 3.00, 'lettuce': 2.43}
+
+order1 = {'cabbage': 2, 'spinach' : 3, 'artichoke': 5}
+
+order_total = 0
+
+for veggie, quantity in order1.items():
+    order_total += veg_dict[veggie] * quantity
+print(order_total)
+
