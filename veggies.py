fb28d537f922f4f8f4531a0d7bcddf8ae24bbe51
deepakk <deepaksingla123@gmail.com>
2022-04-16 22:05:54 +0300

    update veggie prices and add fruits

--- veggies.py
+++ veggies.py
@@ -1,10 +1,12 @@
-veg_dict = {'cabbage': 2.87, 'carrot': 1.59, 'spinach': 3.33, 'asparagus': 2.54, 'artichoke': 3.00, 'lettuce': 2.43}
+veg_dict = {'cabbage': 3.57, 'carrot': 2.99, 'spinach': 4.13, 'asparagus': 3.56, 'artichoke': 4.00, 'lettuce': 3.83}
+fruit_dict = {'peach': 0.65, 'banana': 0.23, 'watermelon': 2.45, 'apple': 0.99, 'dragonfruit': 1.23}
+store_dict = {**veg_dict, **fruit_dict}
 
-order1 = {'cabbage': 2, 'spinach' : 3, 'artichoke': 5}
+order1 = {'cabbage': 2, 'spinach' : 3, 'artichoke': 5, 'apple': 10, 'banana': 7}
 
 order_total = 0
 
-for veggie, quantity in order1.items():
-    order_total += veg_dict[veggie] * quantity
+for item, quantity in order1.items():
+    order_total += store_dict[item] * quantity
 print(order_total)
 
